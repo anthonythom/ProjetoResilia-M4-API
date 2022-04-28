27cdb82f68fa868f957d7b643c98c2ccb2f80b6a
Bruno <b.blanquez@yahoo.com.br>
2022-04-28 16:47:59 -0300

    mudança na pasta infra e app.js

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ const app = express()
 app.use(express.json())
 
 // importa rotas 
-const clientes = require('./controllers/controller-clientes.js')
+const clientes = require('./Clientes/src/controllers/controller-clientes.js')
+const catalogoSimples = require('./Catálogo-Simples/src/controllers/controller-catalogo')
 
-
-// Importa banco de dados fictício
-const bd = require("./infra/bd.js")
+// Importa banco de dados 
+const bd = require("./infra/sqlite-db")
 
 // executa arquivos importados; 
-  // banco de dados (bd) foi integrado e os controllers foram modificados para receberem dados do bd
 clientes(app, bd)
-
+catalogoSimples(app, bd)
 
 app.listen(3030, () => {
   console.log(`Servidor rodando na porta 3030`);
