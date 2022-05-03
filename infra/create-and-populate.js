0053d71d7557621f78faad5454f6ac25f7d0714d
Fulano de TalPeruzini <assamitafael@gmail.com>
2022-05-03 16:06:39 -0300

    Alteração de ID dos Tatuadores para ID-T

--- infra/create-and-populate.js
+++ infra/create-and-populate.js
@@ -115,7 +115,7 @@ function populaAcessorios() {
 
 const  TATUADORES_SCHEMA = `
 CREATE TABLE IF NOT EXISTS "TATUADORES" (
-    "ID" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "ID-T" INTEGER PRIMARY KEY AUTOINCREMENT,
     "NOME" varchar(64),
     "EMAIL" varchar(64),
     "DDD" int(2),
@@ -127,9 +127,9 @@ CREATE TABLE IF NOT EXISTS "TATUADORES" (
   );`;
 //POPULANDO TATUADORES
   const ADD_TATUADORES_DATA  = `
-  INSERT INTO TATUADORES (ID, NOME, EMAIL, DDD, TELEFONE, CPF, RUA, CEP, DATA_NASC)
+  INSERT INTO TATUADORES (ID-T, NOME, EMAIL, DDD, TELEFONE, CPF, RUA, CEP, DATA_NASC)
   VALUES
-      (100B, 'Bruce Waine', 'batman@dc.com.br', '*******', '99', '940028922', '56923423965', 'rua de Gothan', '65485010', '30/05/2001'),
+      (100B, 'Bruce Wayne', 'batman@dc.com.br', '*******', '99', '940028922', '56923423965', 'rua de Gothan', '65485010', '30/05/2001'),
       (200V, 'James Howleat', 'logan2@gmail.com', '********', '98', '940428922', '56223423465', 'rua Melhor naquilo que faz', '65585000', '30/06/1737'),
       (300P, 'Guen Stacy', 'spider@yahoo.com', '********', '97', '950028922', '56923433465', 'rua Marvel', '67485000', '30/04/2001') `
   
@@ -146,12 +146,12 @@ CREATE TABLE IF NOT EXISTS "TATUADORES" (
     
 
 db.serialize( ()=> {
-    criaTabelaClientes();
-    populaClientes();
-    criaTabelaCata();
-    populaTabelaCata();
-    criaTabelaAcessorios();
-    populaAcessorios();
-    //criaTabelaTatuadores
-    //populaTatuadores
+   //criaTabelaClientes();
+  // populaClientes();
+  // criaTabelaCata();
+  // populaTabelaCata();
+  //  criaTabelaAcessorios();
+  //  populaAcessorios();
+    criaTabelaTatuadores
+    populaTatuadores
 });
