cbe88ebddec700aca442e822b6fbe64b36704b18
Yasmin Ramos <yasmincoelhoramos@gmail.com>
2022-05-03 19:03:59 -0300

    create and populate exclusivas

--- Catálogo-Tatuagens Exclusivas/DAO/catalogo-exclusivas-dao.js
+++ Catálogo-Tatuagens Exclusivas/DAO/catalogo-exclusivas-dao.js
@@ -4,13 +4,39 @@ class CatalogoExlusivasDAO {
     }
   
     listarCatalogo() {
-      return new Promise((resolve, reject) => {
-        this.bd.all('select*from Exclusivas', (error, rows) => {
-          error ? reject(`Erro na seleção do banco: ${error}`) : resolve({"Banco de tatuagens exclusivas": rows})
+      return new Promise((resolve, reject)=>{
+        this.bd.all('select*from EXCLUSIVAS', (error, rows)=>{
+          error? reject(`Erro na seleção do banco ${error}`):resolve({"Banco de tatuagens":rows})
         })
       })
     }
   }
+
+  buscaEspecifica(id){
+    return new Promise((resolve, reject)=>{
+      this.bd.all(`select*from EXCLUSIVAS where id = ?`, [id], (error, rows)=>{
+        error?reject(console.log(error)):resolve(rows)
+      })
+    })
+  }
+
+  inserirExclusiva(novaExclusiva){
+    return new Promise((resolve, reject) => {
+      this.bd.run('INSERT INTO EXCLUSIVAS (IMAGEM, TITULO, DESCRICAO, TAMANHO, PRECO, CATEGORIA) VALUES (?, ?, ?, ?, ?, ?)', [novaExclusiva.imagem, novaExclusiva.titulo, novaExclusiva.descricao, novaExclusiva.tamanho, novaExclusiva.preco, novaExclusiva.categoria], 
+      (error) => {
+        error ? reject(error) : resolve('Nova tatuagem inserida com sucesso')
+      })
+    })
+  }
+
+  atualizaExclusivas(atualizacao){
+    return new Promise((resolve, reject)=>{
+      this.bd.run(`update EXCLUSIVAS set imagem=?, titulo=?, descricao=?, tamanho=?, preco=?, catalogo=?, where id=?`, atualizacao, (error)=>{
+        error?reject(error):resolve(`Usuário de ID ${id} deletado`)
+      })
+    })
+  }
+
 
   module.exports= CatalogoExlusivasDAO
   
--- Catálogo-Tatuagens Exclusivas/controllers/controller-catalogo-exclusivas.js
+++ Catálogo-Tatuagens Exclusivas/controllers/controller-catalogo-exclusivas.js
@@ -2,14 +2,16 @@ const Exclusivas = require("../models/model-catalogo-exclusivas")
 const CatalogoExclusivasDAO = require("../dao/catalogo-exclusivas-dao")
 
 const Exclusivas = (app, bd) => {
-  const instDAO = new CatalogoExclusivasDAO
-  app.get('/catalogo-simples', (req, res) => {
-    instDAO.listarCatalogo()
-      .then((resposta) => {
-        res.status(200).json(resposta)
-      }).catch((error) => {
-        res.json(error)
-      })
+  const instDAO = new CatalogoExclusivasDAO(bd)
 
+  app.get('/catalogo-exclusivas', async (req, res)=>{
+  try{
+    const busca = await instDAO
+    listaExclusi
+  }
   })
+
+
+
 }
+
